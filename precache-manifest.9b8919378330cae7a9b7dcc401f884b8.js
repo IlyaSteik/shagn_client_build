217e0e6c741a7f3d4dac8e8092bcf6ba0f257516
Uly <ilya.steik@mail.ru>
2022-08-06 13:54:28 +0300

    first

---
+++ precache-manifest.9b8919378330cae7a9b7dcc401f884b8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b22115a44998c0ccc90397fd28031ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c9e4f5f01b1f32200b5",
+    "url": "./static/css/2.8826781a.chunk.css"
+  },
+  {
+    "revision": "a7cd1ef29f9fbc6b132d",
+    "url": "./static/css/main.f2c9488f.chunk.css"
+  },
+  {
+    "revision": "1c9e4f5f01b1f32200b5",
+    "url": "./static/js/2.cdd25941.chunk.js"
+  },
+  {
+    "revision": "a7cd1ef29f9fbc6b132d",
+    "url": "./static/js/main.79180771.chunk.js"
+  },
+  {
+    "revision": "1b437403f16bd71edadb",
+    "url": "./static/js/runtime-main.9d7e574d.js"
+  },
+  {
+    "revision": "33d12d1456274ec7167a6cb1551a80c7",
+    "url": "./static/media/Boots.33d12d14.svg"
+  },
+  {
+    "revision": "a524701c840afe37aeb56b4a0dfabdeb",
+    "url": "./static/media/Cleats.a524701c.svg"
+  },
+  {
+    "revision": "41aaed9a80dde88ed0bcd7cbb63a522c",
+    "url": "./static/media/Futures.41aaed9a.svg"
+  },
+  {
+    "revision": "4bc886eab44a784387d9326f816e7e25",
+    "url": "./static/media/Jacks.4bc886ea.svg"
+  },
+  {
+    "revision": "bdddffb3bbc27ca1c32bda3eb7d6414c",
+    "url": "./static/media/Power.bdddffb3.svg"
+  },
+  {
+    "revision": "102ced08dfce419a4ab92ff8b16fdd06",
+    "url": "./static/media/SF-Pro-Text-BoldItalic.102ced08.otf"
+  },
+  {
+    "revision": "a7106fbac3d0666ffd315d15b783e8d6",
+    "url": "./static/media/SF-Pro-Text-HeavyItalic.a7106fba.otf"
+  },
+  {
+    "revision": "9f5c9e1ad763571b8513bb88cd4fa23b",
+    "url": "./static/media/SF-Pro-Text-MediumItalic.9f5c9e1a.otf"
+  },
+  {
+    "revision": "90ad050f9579d382bd5fe2e2b85bba26",
+    "url": "./static/media/SF-Pro-Text-RegularItalic.90ad050f.ttf"
+  },
+  {
+    "revision": "b006a42590a5809de6231aa535fabea1",
+    "url": "./static/media/SF-Pro-Text-SemiboldItalic.b006a425.otf"
+  },
+  {
+    "revision": "19b0936f874b9a9157a0874f80419c69",
+    "url": "./static/media/Slams.19b0936f.svg"
+  },
+  {
+    "revision": "7569c4ab527938ec1ba6e1b424d3308c",
+    "url": "./static/media/Slopes.7569c4ab.svg"
+  },
+  {
+    "revision": "285842332772234656cb007a092d0b78",
+    "url": "./static/media/Top.28584233.svg"
+  },
+  {
+    "revision": "9ff7848938101ef17393f022000ce17a",
+    "url": "./static/media/Wales.9ff78489.svg"
+  },
+  {
+    "revision": "eb332429b3d3044f813b8ede77e42dca",
+    "url": "./static/media/empty.eb332429.svg"
+  }
+]);
